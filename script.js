5bb8c01fcd2339e3039405addc1276c933fb47d4
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-11 15:24:06 -0500

    battle of the bands challenge

---
+++ script.js
@@ -0,0 +1,13 @@
+let bandNumber = 0
+
+const takeNumber = function (bandName) {
+  bandNumber++
+  return `${bandNumber}. ${bandName}`
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
+
